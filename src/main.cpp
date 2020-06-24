6dccd064ca75603b5a21702b2193d469d9f19b61
Daniel Lanner <lanner.dan@gmail.com>
2020-06-23 21:26:15 -0400

    texture resizing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,62 @@
 #include <iostream>
 #include <cstdlib>
 #include <png.hpp>
+#include <algorithm>
+
+png::image<png::rgba_pixel> resize_image(png::image<png::rgba_pixel> input)
+{
+
+  const size_t original_width = input.get_width();
+  const size_t original_height = input.get_height();
+
+  const size_t width = original_width / 2;
+  const size_t height = original_height / 2;
+
+  png::image<png::rgba_pixel> image(width, height);
+  
+  size_t originalX = 0;
+  size_t originalY = 0;
+
+  for (size_t y = 0; y < height; ++y, originalY += 2)
+  {
+    originalY = std::min(originalY, original_height - 2);
+
+    for (size_t x = 0, originalX = 0; x < width; ++x, originalX += 2)
+    {
+      originalX = std::min(originalX, original_width - 2);
+
+
+      png::rgba_pixel pxl;
+
+      png::rgba_pixel pxl1 = input.get_pixel(originalX, originalY);
+      png::rgba_pixel pxl2 = input.get_pixel(originalX + 1, originalY);
+      png::rgba_pixel pxl3 = input.get_pixel(originalX, originalY + 1);
+      png::rgba_pixel pxl4 = input.get_pixel(originalX + 1, originalY + 1);
+
+
+      pxl = pxl1;
+
+      if (pxl2.alpha > pxl.alpha)
+      {
+        pxl = pxl2;
+      }
+
+      if (pxl3.alpha > pxl.alpha)
+      {
+        pxl = pxl3;
+      }
+
+      if (pxl4.alpha > pxl.alpha)
+      {
+        pxl = pxl4;
+      }
+
+      image.set_pixel(x, y, pxl);
+    }
+  }
+
+  return image;
+}
 
 int main(int argc, char** argv)
 {
@@ -23,26 +79,30 @@ int main(int argc, char** argv)
   
   try
   {
-    png::image< png::rgb_pixel > image("output.png");
+    png::image< png::rgba_pixel > image("resized.png");
 
-    size_t const width = image.get_width();
-    size_t const height = image.get_height();
+    png::image< png::rgba_pixel > output = resize_image(image);
+    output.write("resized.png");
 
-    for (size_t y = 0; y < height; ++y)
-    {
-      for (size_t x = 0; x < width; ++x)
-      {
-        auto pxl = image[x][y];
 
-        pxl.red = 1.0 - pxl.red;
-        pxl.blue = 1.0 - pxl.blue;
-        pxl.green = 1.0 - pxl.green;
+    //size_t const width = image.get_width();
+    //size_t const height = image.get_height();
 
-        image[x][y] = pxl;
-      }
-    }
+    //for (size_t y = 0; y < height; ++y)
+    //{
+    //  for (size_t x = 0; x < width; ++x)
+    //  {
+    //    auto pxl = image.get_pixel(x, y);
 
-    image.write("output.png");
+    //    pxl.red = 1.0 - pxl.red;
+    //    pxl.blue = 1.0 - pxl.blue;
+    //    pxl.green = 1.0 - pxl.green;
+
+    //    image.set_pixel(x, y, pxl);
+    //  }
+    //}
+
+    //image.write("output.png");
   }
   catch (std::exception const& error)
   {
